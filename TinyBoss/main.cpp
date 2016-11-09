9042fcae1d3e6b1b3ca8568ee662706166460202
Blas <blasvicco@gmail.com>
2016-11-09 09:46:23 -0500

    Initial Commit

---
+++ TinyBoss/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TinyBoss
+//
+//  Created by Blas Eugenio Vicco on 11/9/16.
+//  Copyright © 2016 Blas Eugenio Vicco. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
